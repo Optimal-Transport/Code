20a06eec86da65be635ef36815c18ac4ac84e024
Andrés MT <andres.mt@aim.com>
2020-10-26 01:30:02 +0000

    FB Example

---
+++ FB/prox.m
@@ -0,0 +1,49 @@
+function z = prox(y,m,n, tol)
+% z = prox(y,m,n,tol) : Computes proximal of the indicator function of the 
+% intersection of the simpleces governed by m and n, respectively.
+%
+% z satisfies sum(y,1) ~ m' and sum(y,2) ~ n with tolerance `tol`.
+%
+% By default, tol = 1e-10.
+
+    if nargin < 4
+        tol = 1e-10;
+    end
+    
+    % Recover size of matrix
+    M = length(m);
+    N = length(n);
+    
+    % ? is selected as the midpoint of the interval
+    ep = 0.5;
+    % The initial points for v_1 and v_2 can be built using the information
+    % available already from n and m:
+    v_1 = zeros(M,N);
+    v_2 = zeros(M,N);
+    v_1(1,:) = m;
+    v_2(:,1) = n;
+
+    % Define ? as a fixed value
+    the = 1.5;
+    ith = 1/the;
+
+    % Initialise z:
+    z = y - 0.5 * (v_1 + v_2);
+
+    while indicator_simplex(z, m', 1) + indicator_simplex(z, n, 2) ~= 0.0
+
+        % Update v_1 using its projection over simplex C_1^m
+        v_1 = v_1 + the * (z - project_simplex(ith * v_1 + z, m', 1));
+        % Update v_2 using its projection over simplex C_2^n
+        v_2 = v_2 + the * (z - project_simplex(ith * v_2 + z, n, 2));
+        % Update z
+        z = y - 0.5 * (v_1 + v_2);
+
+        % The indicator evaluation is too hard for double precision arithmetic.
+        % Thus, we relax this condition:
+        if all(abs(sum(z,1) - m') < 1e-10)
+            if all(abs(sum(z,2) - n) < 1e-10)
+               break 
+            end
+        end
+    end

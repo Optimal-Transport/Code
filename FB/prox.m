7bc15aa3e486ed387545c785d99572da858ee17c
Andrés MT <andres.mt@aim.com>
2020-11-03 10:43:43 +0000

    Test

--- FB/prox.m
+++ FB/prox.m
@@ -20,8 +20,8 @@ function z = prox(y,m,n, tol)
     % available already from n and m:
     v_1 = zeros(M,N);
     v_2 = zeros(M,N);
-    v_1(1,:) = m;
-    v_2(:,1) = n;
+    v_1(:,1) = m;
+    v_2(1,:) = n;
 
     % Define ? as a fixed value
     the = 1.5;
@@ -30,19 +30,19 @@ function z = prox(y,m,n, tol)
     % Initialise z:
     z = y - 0.5 * (v_1 + v_2);
 
-    while indicator_simplex(z, m', 1) + indicator_simplex(z, n, 2) ~= 0.0
+    while indicator_simplex(z, m, 2) + indicator_simplex(z, n', 1) ~= 0.0
 
         % Update v_1 using its projection over simplex C_1^m
-        v_1 = v_1 + the * (z - project_simplex(ith * v_1 + z, m', 1));
+        v_1 = v_1 + the * (z - project_simplex(ith * v_1 + z, m, 2) );
         % Update v_2 using its projection over simplex C_2^n
-        v_2 = v_2 + the * (z - project_simplex(ith * v_2 + z, n, 2));
+        v_2 = v_2 + the * (z - project_simplex(ith * v_2 + z, n', 1) );
         % Update z
         z = y - 0.5 * (v_1 + v_2);
 
         % The indicator evaluation is too hard for double precision arithmetic.
         % Thus, we relax this condition:
-        if all(abs(sum(abs(z),1) - m') < 1e-10)
-            if all(abs(sum(abs(z),2) - n) < 1e-10)
+        if all(abs(sum(abs(z),1) - n') < 1e-10)
+            if all(abs(sum(abs(z),2) - m) < 1e-10)
                 z = abs(z);
                break 
             end

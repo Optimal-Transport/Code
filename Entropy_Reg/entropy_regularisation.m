bf639f1a962390e132b9cc36b7cba9a370ef34a2
PAWhalley <73125898+PAWhalley@users.noreply.github.com>
2020-11-25 15:01:27 +0000

    Entropic Regularisation

--- Entropy_Reg/entropy_regularisation.m
+++ Entropy_Reg/entropy_regularisation.m
@@ -7,27 +7,30 @@ function [gam,obj,temp] = entropy_regularisation(m,n,c,eps)
     iters = 1000; %Number of iterations.
     
     % Fetch lengths of m and n
-    N = length(m);
-    M = length(n);
+    N = length(n);
+    M = length(m);
     %Save that objective function
     
 
     
     Geps = exp(-c/eps);
     %Gibbs Kernel
+    %disp(size(Geps))
     
     cons = 1/sum(sum(Geps));
     a = ones(M,1)*cons;
     b = ones(N,1)*cons;
+    %disp(size(a))
+    %disp(size(b))
     %Initialisations
     
     GepsT = Geps.';
     %Initialising the transpose
     
     
-    gam = (b.*((a.*Geps).')).'; 
-    obj = [sum(c*gam,'all')];
-    %initial objective calculation
+    %gam = (b.*((a.*Geps).'));
+    %obj = [sum(c*gam,'all')];
+    %%initial objective calculation
     
     
     
@@ -39,14 +42,16 @@ function [gam,obj,temp] = entropy_regularisation(m,n,c,eps)
         b = n./(GepsT*a); 
         %updating a and b
         
-        gam = (b.*((a.*Geps).')).';
-        obj(end+1) = sum(c*gam,'all');
-        %objective calculation.
+        %gam = (b.*((a.*Geps).'));
+        %obj(end+1) = sum(c*gam,'all');
+        %%objective calculation.
         
     end
+    gam = (b.*((a.*Geps).')); %final gamma
+    obj = sum(c*gam,'all'); 
+    %calculating final objective
     
-    
-    gam = (b.*((a.*Geps).')).';%final gam
+   
     
     
     temp = toc(tStart); %total time

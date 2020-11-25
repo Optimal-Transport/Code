97adbb0a994d435c120e704af05bb6ffafb2112c
Andrés MT <andres.mt@aim.com>
2020-11-25 20:12:54 +0000

    Fix in Entropic Regularisation

--- FB/entropy_regularisation.m
+++ FB/entropy_regularisation.m
@@ -1,4 +1,4 @@
-function [gam,obj,temp] = Entropy_Regularisation(m,n,c,eps)
+function [x,obj,temp] = Entropy_Regularisation(m,n,c,eps)
 %%
 % Entropy_Regularisation(m,n,c,eps) : Executes the Entropy Regularisation
 % algorithm for problems on optimal transport.
@@ -25,35 +25,30 @@ function [gam,obj,temp] = Entropy_Regularisation(m,n,c,eps)
     % Compute Gibbs Kernel
     Geps = exp(-c/eps);
     
-    %Initialisations
+    % Initialisations
     cons  = 1/sum(sum(Geps));
     a     = ones(M,1) * cons;
     b     = ones(N,1) * cons;
-    GepsT = Geps.';
-    %Initialising the transpose
-    
     
     %gam = (b.*((a.*Geps).'));
     %obj = [sum(c*gam,'all')];
     %%initial objective calculation
     
-    %starting time.
+    % Starting time
     tStart = tic;
     for i = 1:iters
+        % Updating a and b
         a = m./(Geps*b);
-        b = n./(GepsT*a); 
-        %updating a and b
+        b = n./(a'*Geps)';
         
         %gam = (b.*((a.*Geps).'));
         %obj(end+1) = sum(c*gam,'all');
-        %%objective calculation.
         
     end
     % Update time clock
     temp = toc(tStart);
+    % Final estimation
+    x   = a.*Geps .* b';
+    obj = sum(c .* x,'all'); 
     
-    % Final gamma
-    gam = (b.*((a.*Geps).')); 
-    obj = sum(c .* gam,'all'); 
-     
 end
